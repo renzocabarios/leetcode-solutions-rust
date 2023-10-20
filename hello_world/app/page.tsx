a04f970b93ce57cfc1db2280e1cbd9afff80f7b4
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-10-20 23:22:23 +0800

    Add hello_world

---
+++ hello_world/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-3xl">Hello World</h1>
+    </main>
+  );
+}
